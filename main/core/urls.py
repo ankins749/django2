58310f8e2f42602f1e8b53d30707b686bb000d02
ankins749 <78509370+ankins749@users.noreply.github.com>
2024-05-23 09:45:22 -0400

    fix

--- main/core/urls.py
+++ main/core/urls.py
@@ -1,8 +1,25 @@
 from django.urls import path
-from .views import index, nosotros
+from .views import index, nosotros, admin , agregar_producto, agregar_user , api, boleta,carrito_compras,historial_compras,mantenedor_bodega,mis_datos,navbar,nosotros
+from .views import registro,store,ventas
+
 
 
 urlpatterns = [
     path('', index, name="index"),
     path('nosotros', nosotros, name="nosotros"),
+    path('admin',admin, name="admin"),
+    path('agregar_producto', agregar_producto, name="agregar_productos"),
+    path('agregar_user', agregar_user, name="agregar_user"),
+    path('api', api, name="api"),
+    path('boleta', boleta, name="boleta"),
+    path('carrito_compras', carrito_compras, name="carrito_compras"),
+    path('historial_compras', historial_compras, name="historial_compras"),
+    path('mantenedor_bodega', mantenedor_bodega, name="mantenedor_bodega"),
+    path('mismis_datos',mis_datos, name="mismis_datos"),
+    path('navbar',navbar, name="navbar"),
+    path('nosotros',nosotros, name="nosotros"),
+    path('registro',registro, name="registro"),
+    path('store',store, name="store"),
+    path('ventas',ventas, name="ventas"),
+
 ]
--- main/core/views.py
+++ main/core/views.py
@@ -5,5 +5,36 @@ def index(request):
 
 def nosotros(request):
     return render(request, 'core/nosotros.html')
+def admin(request):
+    return render(request, 'core/admin.html')
+def agregar_producto(request):
+    return render(request, 'core/agregar_producto.html')
+def agregar_user(request):
+    return render(request, 'core/agregar_user.html')
+def api(request):
+    return render(request, 'core/api.html')
+def boleta(request):
+    return render(request, 'core/boleta.html')
+def carrito_compras(request):
+    return render(request, 'core/carrito_compras.html')
+def historial_compras(request):
+    return render(request, 'core/historial_compras.html')
+def mantenedor_bodega(request):
+    return render(request, 'core/mantenedor_bodega.html')
+def mis_datos(request):
+    return render(request, 'core/mis_datos.html')
+def navbars(request):
+    return render(request, 'core/navbars.html')
+def nosotros(request):
+    return render(request, 'core/nosotros.html')
+def registro(request):
+    return render(request, 'core/registro.html')
+def store(request):
+    return render(request, 'core/store.html')
+def ventas(request):
+    return render(request, 'core/ventas.html')
+
+
+
 
 # Create your views here.
